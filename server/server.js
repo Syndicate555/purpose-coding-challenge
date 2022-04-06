900e279912021b6624e1c0ce7ff17d30c3edb244
Saffat Aziz <saziz028@uottawa.ca>
2022-04-06 14:49:47 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -127,7 +127,7 @@ app.post('/submit', async (req, res) => {
   }
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4545
 
 app.listen(PORT, () => {
   console.log(`Server Listening on ${PORT}`)
